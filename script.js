6d85c52c907916dad04ca6b5a0a799f5358dbb14
Aleks Kimball-Moreno <alekskimball@gmail.com>
2020-12-21 10:40:51 -0800

    added ajax method to pull from api and added id's to card paragraphs

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-var apiKey = "18d48c5ceb5dd195c9b4f608772f3693";
+var apiKey = "6006bbb67ca60a0e3ed662e5aa195ac5";
 
 var urlQuery = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=" + apiKey;
+
+$.ajax({
+    url: urlQuery,
+    method: "GET"
+}).then(function (response) {
+    console.log(urlQuery)
+
+
+})
