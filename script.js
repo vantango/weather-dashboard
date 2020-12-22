c5906553471fd65204b66a1d46f2c724f1839cc5
Aleks Kimball-Moreno <alekskimball@gmail.com>
2020-12-21 19:25:40 -0800

    added function with for loop for five day forecast

--- script.js
+++ script.js
@@ -5,25 +5,40 @@ function currentWeather(cityName) {
 
     var urlQuery = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
 
-
+    // Calling current city weather details
     $.ajax({
         url: urlQuery,
         method: "GET"
     }).then(function (response) {
-        console.log(urlQuery)
-        console.log(response)
         $("#city-name").text(response.name)
-        $("#temp").text("Temperature: " + response.main.temp + " F")
+        $("#temp").text("Temperature: " + response.main.temp + " °F")
         $("#humidity").text("Humidity: " + response.main.humidity + " %")
         $("#wind-speed").text("Wind Speed: " + response.wind.speed + " MPH")
+
         // Calling UV Index API
         var longitude = response.coord.lon
         var latitude = response.coord.lat
         uvIndex(longitude, latitude)
+
         // Calling 5-Day Forecast
+        var fiveDayQuery = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
+        $.ajax({
+            url: fiveDayQuery,
+            method: "GET"
+        }).then(function (fiveDayResponse) {
+            console.log(fiveDayResponse)
+            for (var i = 0; i < fiveDayResponse.list.length; i++) {
+                // const element = array[index];
+                console.log(fiveDayResponse.list[i])
+                // if {
+                //     // .indexOf
+                // }
+            }
+        })
     })
 }
 
+// Calling the UV Index
 function uvIndex(longitude, latitude) {
     var uvQuery = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
     $.ajax({
@@ -32,6 +47,9 @@ function uvIndex(longitude, latitude) {
     }).then(function (uvResponse) {
         console.log(uvResponse)
         $("#uv-index").text("UV Index: " + uvResponse.value)
+        // if {
+        // Set color depending on uv index value
+        // }
     })
 }
 
