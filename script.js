3d505d4c48b407e6d9592ca9ad81ba8aeb595b51
Aleks Kimball-Moreno <alekskimball@gmail.com>
2020-12-21 22:55:28 -0800

    added 5 day forecast functionality to cards for date, temp, and humidity

--- script.js
+++ script.js
@@ -15,26 +15,80 @@ function currentWeather(cityInput) {
         $("#temp").text("Temperature: " + response.main.temp + " °F")
         $("#humidity").text("Humidity: " + response.main.humidity + " %")
         $("#wind-speed").text("Wind Speed: " + response.wind.speed + " MPH")
-
         // Calling UV Index API
         var longitude = response.coord.lon
         var latitude = response.coord.lat
         uvIndex(longitude, latitude)
 
         // Calling 5-Day Forecast
-        var fiveDayQuery = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=" + apiKey;
+        var fiveDayQuery = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=" + apiKey + "&units=imperial";
         $.ajax({
             url: fiveDayQuery,
             method: "GET"
         }).then(function (fiveDayResponse) {
+
+            // Adds date to each individual forecast card
+            var cardDate = fiveDayResponse.list[1].dt_txt
+            $("#card-date1").text(cardDate.slice(0, 10));
+
+            cardDate = fiveDayResponse.list[8].dt_txt
+            $("#card-date2").text(cardDate.slice(0, 10));
+
+            cardDate = fiveDayResponse.list[16].dt_txt
+            $("#card-date3").text(cardDate.slice(0, 10));
+
+            cardDate = fiveDayResponse.list[24].dt_txt
+            $("#card-date4").text(cardDate.slice(0, 10));
+
+            cardDate = fiveDayResponse.list[32].dt_txt
+            $("#card-date5").text(cardDate.slice(0, 10));
+
+            // Adds temperature to each individual forecast card
+            var cardTemp = fiveDayResponse.list[1].main.temp
+            $("#card-temp1").text(cardTemp + " °F");
+
+            cardTemp = fiveDayResponse.list[8].main.temp
+            $("#card-temp2").text(cardTemp + " °F");
+
+            cardTemp = fiveDayResponse.list[16].main.temp
+            $("#card-temp3").text(cardTemp + " °F");
+
+            cardTemp = fiveDayResponse.list[24].main.temp
+            $("#card-temp4").text(cardTemp + " °F");
+
+            cardTemp = fiveDayResponse.list[32].main.temp
+            $("#card-temp5").text(cardTemp + " °F");
+
+            // Adds temperature to each individual forecast card
+            var cardHumidity = fiveDayResponse.list[1].main.humidity
+            $("#card-humidity1").text("Humidity: " + cardHumidity);
+
+            cardHumidity = fiveDayResponse.list[8].main.humidity
+            $("#card-humidity2").text("Humidity: " + cardHumidity);
+
+            cardHumidity = fiveDayResponse.list[16].main.humidity
+            $("#card-humidity3").text("Humidity: " + cardHumidity);
+
+            cardHumidity = fiveDayResponse.list[24].main.humidity
+            $("#card-humidity4").text("Humidity: " + cardHumidity);
+
+            cardHumidity = fiveDayResponse.list[32].main.humidity
+            $("#card-humidity5").text("Humidity: " + cardHumidity);
             console.log(fiveDayResponse)
-            for (var i = 0; i < fiveDayResponse.list.length; i++) {
-                // const element = array[index];
-                // console.log(fiveDayResponse.list[i])
-                // if {
-                //     // .indexOf
-                // }
-            }
+
+
+
+
+
+
+            // for (var i = 0; i < fiveDayResponse.list.length; i++) {
+            //     // const element = array[index];
+            //     console.log(fiveDayResponse.list[i])
+            //     // if {
+            //     //     // .indexOf
+            //     // }
+            // }
+
         })
     })
 }
